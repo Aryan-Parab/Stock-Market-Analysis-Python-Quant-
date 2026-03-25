062b758df147167cbaddcc85934ab1b6235e91d9
Aryan Parab <aryanparab.ca@outlook.com>
2026-03-25 12:42:42 -0700

    Data Pipeline

--- Data_Pipeline/phase1/fetch_data.py
+++ Data_Pipeline/phase1/fetch_data.py
@@ -1,8 +1,7 @@
 from tkinter import FALSE
 import colorlog
-from anyio import Path
+#from anyio import Path
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 import requests
