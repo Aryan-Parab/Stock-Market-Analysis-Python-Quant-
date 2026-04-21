3e3cf51b50e28a8653e71ac0752d3835a62d8aae
Aryan Parab <86849576+Aryan-Parab@users.noreply.github.com>
2026-04-21 19:22:19 +0000

    Working on phase1

---
+++ Eda/phase1/summary_report.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplolib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import seaborn as sns
+
+# Metrics to include in the report 
+'''
+1. Mean daily return
+2. Standard deviation of daily returns
+3.  sharpe ratio, max drawdown, volatility , annuralized return,
+4. Corrlation matrix of returns, p/e ratio,peg ratio, dividend yield
+5. Visualizations: histogram, find outliers using box plot, time series data, scatter plot of returns, heatmap of correlation 
+'''
+
+# Risk metrics to include in the report
+'''
+1. Variance, and Std Deviation
+2. Skewness -> negative (fat left tail) = crash risk market,
+3. Kurtosis -> >3 ( fat tails) = higher probability of extreme events.
+'''
+
+# Ratio metrics to include in the report
+#  
